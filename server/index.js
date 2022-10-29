3aeb80b7eb3043e1bb2da49a1b90a03eb88da9ae
ahmedm512 <1600155@eng.asu.edu.eg>
2022-10-29 11:56:21 +0200

    Project and Client RootQuiries

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const colors = require('colors');
+require('dotenv').config();
+const cors = require('cors')
+const {graphqlHTTP} = require('express-graphql');
+const schema = require('./schema/schema.js');
+const connectDB = require('./config/db.js');
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+app.use('/graphql', graphqlHTTP({
+schema,
+graphiql: process.env.NODE_ENV === 'development',
+}))
+
+app.use(cors);
+
+app.listen(port, console.log(`Server is running on port ${port}`));
